86e3b5dcaec8a8994064b7a7cf27617c452431cf
maochang <55223073+mcku1115@users.noreply.github.com>
2025-03-14 00:39:09 +0800

    Updates

---
+++ assets/2025-CM6r_1Cc.js
@@ -0,0 +1,1 @@
+const t=[{title:"Laurent Prevot, Sheng-Fu Wang, Jou-An Chi and Shu-Kai Hsieh (2025). Extending the BabyLM Initiative : Promoting Diversity in Datasets and Metrics through High-Quality Linguistic Corpora."}];export{t as default};
