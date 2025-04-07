eaf37d1c17ee533fc276b20f424f38454447e8ae
maochang <55223073+mcku1115@users.noreply.github.com>
2025-04-07 15:56:49 +0800

    update publication

--- libs/client.ts
+++ libs/client.ts
@@ -1,6 +1,17 @@
-import { createClient } from "microcms-js-sdk";
+import { createClient, MicroCMSClient } from 'microcms-js-sdk';
 
-export const client = createClient({
-  serviceDomain: import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN || "",
-  apiKey: import.meta.env.VITE_MICROCMS_API_KEY || "",
-});
+const domain = import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN;
+const apiKey = import.meta.env.VITE_MICROCMS_API_KEY;
+
+let client: MicroCMSClient | null = null;
+
+if (!domain || !apiKey) {
+  console.warn("⚠️ microCMS 初始化失敗：未設定環境變數");
+} else {
+  client = createClient({
+    serviceDomain: domain,
+    apiKey: apiKey,
+  });
+}
+
+export { client };
